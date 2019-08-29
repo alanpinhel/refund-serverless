241ee7e1e257bf8cd1a54ed481e89591c08b6615
Alan Pinhel <alanpinhel@gmail.com>
2019-08-29 11:29:41 -0300

    add material

--- src/app/refunds/refunds.module.ts
+++ src/app/refunds/refunds.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
+import { SharedModule } from '@app/shared'
 import { RefundsRoutingModule } from './refunds-routing.module'
 import { RefundsComponent } from './refunds/refunds.component'
 
 @NgModule({
   declarations: [RefundsComponent],
-  imports: [CommonModule, RefundsRoutingModule],
+  imports: [CommonModule, RefundsRoutingModule, SharedModule],
 })
 export class RefundsModule {}
