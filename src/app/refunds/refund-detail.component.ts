87237a4716af793bdb378f9360590912c0f6290b
Alan Pinhel <alanpinhel@gmail.com>
2019-09-26 17:59:53 -0300

    refactoring css using bemcss

--- src/app/refunds/refund-detail.component.ts
+++ src/app/refunds/refund-detail.component.ts
@@ -2,68 +2,74 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core'
 
 import { Refund, MasterDetailCommands } from '@app/core'
 
+// <div class="main">
+//       <div class="amount-expenses uppercase">
+//         <p *ngIf="refund?.expenses?.length">{{ refund.expenses.length }} expenses in your refund</p>
+//         <p *ngIf="!refund?.expenses?.length">refund without expenses</p>
+//       </div>
+
+//       <mat-action-list>
+//         <mat-divider></mat-divider>
+//         <mat-list-item *ngFor="let expense of refund.expenses">
+//           <img matListAvatar src="https://nayrontoledo.files.wordpress.com/2014/04/dcd4c-comprovanteqnadacomprova.jpg" />
+//           <div mat-line style="display: flex">
+//             <span>{{ expense.type }}</span>
+//             <span class="spacer"></span>
+//             <span>{{ expense.value | currency }}</span>
+//             <mat-divider></mat-divider>
+//           </div>
+//           <p mat-line>{{ expense.date | date }}</p>
+//         </mat-list-item>
+//       </mat-action-list>
+//     </div>
+
+//     <div class="actions">
+//       <p>{{ refund.date | date }}</p>
+//       <p>{{ refund.status }}</p>
+//       <p>{{ refund.reason }}</p>
+//     </div>
+
 @Component({
   selector: 'app-refund-detail',
   template: `
-    <div class="wrapper">
-      <mat-toolbar color="primary">
-        <mat-toolbar-row>
-          <button mat-icon-button (click)="handleBack()">
-            <mat-icon>arrow_back</mat-icon>
-          </button>
-          <span class="spacer"></span>
-          <button mat-icon-button *ngIf="showActions()" (click)="handleDelete()">
-            <mat-icon>delete</mat-icon>
-          </button>
-        </mat-toolbar-row>
-
-        <mat-toolbar-row style="position: relative;">
-          <span style="margin-left: 56px;">Refund</span>
-          <button mat-mini-fab color="" style="position: absolute; bottom: -20px;" aria-label="New Expense">
-            <mat-icon>add</mat-icon>
-          </button>
-        </mat-toolbar-row>
-      </mat-toolbar>
-
-      <div class="main">
-        <div class="amount-expenses uppercase">
-          <p *ngIf="refund?.expenses?.length">{{ refund.expenses.length }} expenses in your refund</p>
-          <p *ngIf="!refund?.expenses?.length">refund without expenses</p>
-        </div>
-
-        <mat-action-list>
-          <mat-divider></mat-divider>
-          <mat-list-item *ngFor="let expense of refund.expenses">
-            <img matListAvatar src="https://nayrontoledo.files.wordpress.com/2014/04/dcd4c-comprovanteqnadacomprova.jpg" />
-            <div mat-line style="display: flex">
-              <span>{{ expense.type }}</span>
-              <span class="spacer"></span>
-              <span>{{ expense.value | currency }}</span>
-              <mat-divider></mat-divider>
-            </div>
-            <p mat-line>{{ expense.date | date }}</p>
-          </mat-list-item>
-        </mat-action-list>
-      </div>
-
-      <div class="actions">
-        <p style="text-align: right;">Total {{ refund.total | currency }}</p>
-        <p>{{ refund.date | date }}</p>
-        <p>{{ refund.status }}</p>
-        <p>{{ refund.reason }}</p>
-
-        <button
-          mat-flat-button
-          class="full uppercase"
-          color="primary"
-          *ngIf="showActions()"
-          [disabled]="disableActionConfirm()"
-          (click)="handleConfirm()"
-        >
-          Confirm
+    <mat-toolbar color="primary">
+      <mat-toolbar-row>
+        <button mat-icon-button (click)="handleBack()">
+          <mat-icon>arrow_back</mat-icon>
         </button>
+        <span class="spacer"></span>
+        <button mat-icon-button *ngIf="showActions()" (click)="handleDelete()">
+          <mat-icon>delete</mat-icon>
+        </button>
+      </mat-toolbar-row>
+    </mat-toolbar>
+
+    <mat-toolbar class="refundDetail__priorityActionsToolbar" color="primary">
+      <mat-toolbar-row>
+        <span class="refundDetail__title">Refund</span>
+        <button mat-mini-fab class="refundDetail__fabNewExpense" color="" aria-label="New Expense">
+          <mat-icon>add</mat-icon>
+        </button>
+      </mat-toolbar-row>
+    </mat-toolbar>
+
+    <footer class="refundDetail__footer">
+      <div class="refundDetail__totalWrapper">
+        <span class="refundDetail__totalLabel">Total</span>
+        <span class="refundDetail__totalValue">{{ refund.total | currency }}</span>
       </div>
-    </div>
+
+      <button
+        mat-flat-button
+        class="refundDetail__confirmButton"
+        color="primary"
+        *ngIf="showActions()"
+        [disabled]="disableActionConfirm()"
+        (click)="handleConfirm()"
+      >
+        Confirm
+      </button>
+    </footer>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
