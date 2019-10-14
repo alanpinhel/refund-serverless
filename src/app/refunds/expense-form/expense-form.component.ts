7c9cf48549136afca6fcd5c94f75234bb5930111
Alan Pinhel <alanpinhel@gmail.com>
2019-10-14 16:23:53 -0300

    expense form

--- src/app/refunds/expense-form/expense-form.component.ts
+++ src/app/refunds/expense-form/expense-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core'
+import { Component, ChangeDetectionStrategy, Input, OnInit, ViewChild, ElementRef } from '@angular/core'
+import { Validators, FormBuilder } from '@angular/forms'
 
-import { MasterDetailCommands, Expense } from '@app/core'
+import { MasterDetailCommands, Expense, Refund } from '@app/core'
 
 @Component({
   selector: 'app-expense-form',
@@ -8,6 +9,37 @@ import { MasterDetailCommands, Expense } from '@app/core'
   styleUrls: ['./expense-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExpenseFormComponent {
+export class ExpenseFormComponent implements OnInit {
+  @Input() refund: Refund
   @Input() commands: MasterDetailCommands<Expense>
+
+  @ViewChild('date', { static: true }) dateElement: ElementRef
+
+  form = this.fb.group({
+    date: ['', Validators.required],
+    type: ['', Validators.required],
+    amount: ['', Validators.required],
+  })
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.setFocus()
+  }
+
+  close() {
+    this.commands.close()
+  }
+
+  saveExpense() {
+    const { valid, value } = this.form
+    if (valid) {
+      this.close()
+      this.commands.add({ ...value, date: new Date(value.date).getTime(), refund: this.refund.id })
+    }
+  }
+
+  setFocus() {
+    this.dateElement.nativeElement.focus()
+  }
 }
